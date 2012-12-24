33c708f4497e7d93958bc0cd7ad1ba707cd67cc7
mangaroo@gmail.com <mangaroo@gmail.com@f75565ed-ee52-8789-1686-a88207c6b37a>
2012-12-24 01:31:35 +0000

    revise compile comments for compilerobotremoteserver.bat

--- trunk/compilerobotremoteserver.bat
+++ trunk/compilerobotremoteserver.bat
@@ -1,14 +1,16 @@
+@echo off
 rem .NET command line compiler example for compiling remote server & example library
-rem You may alternatively compile with SharpDevelop, Visual Studio, etc. instead
+rem You may alternatively compile with SharpDevelop, MonoDevelop, Visual Studio, etc. instead
 rem by creating new console application and/or class library projects with the source files
+
+rem change .NET framework path to desired target version as needed
 
 rem compile robotremoteserver (from local path)
 rem assumes XML-RPC.NET library DLL is precompiled and in local path
-rem FYI get XML-RPC.NET library from
-rem www.xml-rpc.net
-rem change .NET framework path to desired version as needed
-
+rem FYI get XML-RPC.NET library from www.xml-rpc.net
+@echo on
 %windir%\Microsoft.NET\Framework\v2.0.50727\csc.exe /out:robotremoteserver.exe /target:exe /reference:CookComputing.XmlRpcV2.dll robotremoteserver.cs
-
+@echo off
 rem compile examplelibrary with generated XML documentation
+@echo on
 %windir%\Microsoft.NET\Framework\v2.0.50727\csc.exe /out:examplelibrary.dll /target:library /doc:examplelibrary_doc.xml examplelibrary.cs
