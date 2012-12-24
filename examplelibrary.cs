e80008d4af4842c068ff84405f341e64780a33f3
mangaroo@gmail.com <mangaroo@gmail.com@f75565ed-ee52-8789-1686-a88207c6b37a>
2012-12-24 04:53:29 +0000

    Update to XML documentation, and remove debug code.

--- examplelibrary.cs
+++ examplelibrary.cs
@@ -8,34 +8,26 @@ namespace RobotFramework
 	/// RobotFramework example of .NET Implementation of a remote library.
 	/// Based on the Python example at http://robotframework.googlecode.com/svn/trunk/tools/remoteserver/example/examplelibrary.py
 	/// Compile into an assembly DLL and pass filename and "RobotFramework.ExampleRemoteLibrary"
-	/// as arguments to the .NET implementation of remoteserver
+	/// as arguments to the .NET implementation of remote server
 	/// to start the server with this library.
 	/// </summary>
 	public class ExampleRemoteLibrary
 	{
 		/// <summary>
 		/// Example keyword that validates 2 given strings match.
-		/// 
 		/// Throws exception (as a way to fail) when strings don't match.
-		/// Logs comparing of 2 strings, but remote library won't pass on
-		/// the logged data to Robot Framework, known limitation.
+		/// Logs comparing of 2 strings to keyword output (standard output redirected).
 		/// </summary>
 		/// <param name="str1">1st string to compare</param>
 		/// <param name="str2">2nd string to compare</param>
 		public void strings_should_be_equal(string str1, string str2)
 		{
 			Console.WriteLine("Comparing '{0}' to '{1}'",str1,str2);
-			if(str1 != str2){
-				Console.WriteLine("not equal"); 
-				//fails below w/ XML-RPC fault code 0, Object reference not set to an instance of an object.
-				throw new Exception("Given strings are not equal");
-			}
-			
+			if(str1 != str2) throw new Exception("Given strings are not equal");			
 		}
 		
 		/// <summary>
 		/// Example keyword that returns # of items in a given directory.
-		/// 
 		/// Items include folders, subfolders, and files, etc.
 		/// </summary>
 		/// <param name="path">Directory to get # of items for</param>
